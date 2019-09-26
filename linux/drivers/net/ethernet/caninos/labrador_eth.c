f93396b105d09a06d96a9e10764568a787b29d76
r0zbot <r0zbot@users.noreply.github.com>
2019-09-26 17:12:09 -0300

    open() e setup()

--- linux/drivers/net/ethernet/caninos/labrador_eth.c
+++ linux/drivers/net/ethernet/caninos/labrador_eth.c
@@ -52,8 +52,33 @@
 #define TX_RING_SIZE 128 
 #define ETH_PKG_MAX 1518
 
-#define LABRADOR_INT_STATUS(x)  (x + 0x0028)
-#define LABRADOR_INT_ENABLE(x)  (x + 0x0038)
+#define EC_BMODE_SWR (0x1) // software reset
+#define EC_CACHETHR_CPTL(x) (((x) & 0xFF) << 24) // cache pause threshold level
+#define EC_CACHETHR_CRTL(x) (((x) & 0xFF) << 16) // cache restart threshold level
+#define EC_CACHETHR_PQT(x)  ((x) & 0xFFFF) // flow control pause quanta time
+#define EC_FIFOTHR_FPTL(x) (((x) & 0xFFFF) << 16) // fifo pause threshold level
+#define EC_FIFOTHR_FRTL(x) ((x) & 0xFFFF) // fifo restart threshold level
+#define EC_FLOWCTRL_ENALL (0x1F << 27)
+#define EC_OPMODE_FD  (0x1 << 9) // full duplex mode
+#define EC_OPMODE_SPEED(x) (((x) & 0x3) << 16) // eth speed selection
+#define EC_OPMODE_PR (0x1 << 6) // promiscuous mode
+#define EC_OPMODE_ST (0x1 << 13) // start or stop transmit command
+#define EC_OPMODE_SR (0x1 << 1) // start or stop receive command
+#define EC_OPMODE_10M (0x1 << 17) // set when work on 10M, otherwise 100M
+#define EC_IEN_ALL (0x1CDE3) // TU interrupt disabled
+#define EC_TXPOLL_ST (0x1) // leave suspended mode to running mode to start xmit
+#define EC_RXPOLL_SR (0x1) // leave suspended to running mode
+
+#define LABRADOR_MAC_CTRL(x)  (x + 0x00B0)
+
+#define LABRADOR_INT_STATUS(x)  (x + 0x0028) //CSR5
+#define LABRADOR_MAC_CSR6(x)  (x + 0x0030)
+#define LABRADOR_INT_ENABLE(x)  (x + 0x0038) //CSR7
+#define LABRADOR_MAC_CSR10(x)  (x + 0x0050)
+#define LABRADOR_MAC_CSR11(x)  (x + 0x0058)
+#define LABRADOR_MAC_CSR18(x)  (x + 0x0090)
+#define LABRADOR_MAC_CSR19(x)  (x + 0x0098)
+#define LABRADOR_MAC_CSR20(x)  (x + 0x00A0)
 
 #define INFO_MSG(fmt,...) pr_info(MODNAME ": " fmt, ##__VA_ARGS__)
 #define ERR_MSG(fmt,...) pr_err(MODNAME ": " fmt, ##__VA_ARGS__)
@@ -99,6 +124,82 @@ struct rx_status_t {
     __le32 statushashcrc;
 };
 
+static int ethernet_mac_setup(void __iomem *regbase)
+{
+    // hardware soft reset and set bus mode
+    INFO_MSG(" ethernet_mac_setup(void)");
+    
+    writel(readl(regbase) | EC_BMODE_SWR, regbase);
+    
+    do {
+        udelay(10);
+    } while (readl(regbase) & EC_BMODE_SWR);
+    
+    // select clk input from external phy
+
+    writel(readl(LABRADOR_MAC_CTRL(regbase)) | (0x1 << 1), LABRADOR_MAC_CTRL(regbase));
+    
+    writel(0, LABRADOR_MAC_CSR10(regbase));
+    
+    writel(0xcc000000, LABRADOR_MAC_CSR10(regbase));
+    
+    // set flow control mode and force transmiter to pause about 100ms
+    
+    writel(EC_CACHETHR_CPTL(0x0) | EC_CACHETHR_CRTL(0x0) | 
+               EC_CACHETHR_PQT(0x4FFF), LABRADOR_MAC_CSR18(regbase));
+    
+    writel(EC_FIFOTHR_FPTL(0x40) | EC_FIFOTHR_FRTL(0x10), LABRADOR_MAC_CSR19(regbase));
+    
+    writel(EC_FLOWCTRL_ENALL, LABRADOR_MAC_CSR20(regbase));
+    
+    // always set to work as full duplex because of a MAC bug
+    
+    writel(readl(LABRADOR_MAC_CSR6(regbase)) | EC_OPMODE_FD, LABRADOR_MAC_CSR6(regbase)); //Full-duplex
+    
+    writel(readl(LABRADOR_MAC_CSR6(regbase)) & ~EC_OPMODE_10M, LABRADOR_MAC_CSR6(regbase)); //100M
+    
+    writel(readl(LABRADOR_MAC_CSR6(regbase)) & (~EC_OPMODE_PR), LABRADOR_MAC_CSR6(regbase));
+    
+    //interrupt mitigation control register
+    //NRP =7, RT =1, CS=0
+    
+    writel(0x004e0000, LABRADOR_MAC_CSR11(regbase));
+    
+    // stop tx and rx and disable interrupts
+    
+    writel(readl(LABRADOR_MAC_CSR6(regbase)) & (~(EC_OPMODE_ST | EC_OPMODE_SR)), LABRADOR_MAC_CSR6(regbase));
+    writel(0, LABRADOR_INT_ENABLE(regbase));
+    
+    return 0;
+}
+
+static int labrador_eth_open(struct net_device *ndev)
+{
+    struct netdata_local *pldat = netdev_priv(ndev);
+    int ret;
+
+    if (netif_msg_ifup(pldat))
+        dev_dbg(&pldat->pdev->dev, "enabling %s\n", ndev->name);
+
+    ret = clk_prepare_enable(pldat->clk);
+    if (ret)
+        return ret;
+
+    /* Suspended PHY makes LPC ethernet core block, so resume now */
+    phy_resume(ndev->phydev);
+
+    /* Reset and initialize */
+    ethernet_mac_setup(pldat);
+
+    /* schedule a link state check */
+    phy_start(ndev->phydev);
+    netif_start_queue(ndev);
+    napi_enable(&pldat->napi);
+
+    return 0;
+}
+
+
 static void labrador_eth_disable_int(void __iomem *regbase)
 {
     writel(0, LABRADOR_INT_ENABLE(regbase));
@@ -129,6 +230,25 @@ static int labrador_eth_drv_remove(struct platform_device *pdev)
 {
     INFO_MSG("labrador_eth_drv_remove!");
 }
+
+static const struct ethtool_ops labrador_eth_ethtool_ops = {
+    // .get_drvinfo    = labrador_eth_ethtool_getdrvinfo,
+    // .get_msglevel    = labrador_eth_ethtool_getmsglevel,
+    // .set_msglevel    = labrador_eth_ethtool_setmsglevel,
+    // .get_link    = ethtool_op_get_link,
+    // .get_link_ksettings = phy_ethtool_get_link_ksettings,
+    // .set_link_ksettings = phy_ethtool_set_link_ksettings,
+};
+
+static const struct net_device_ops labrador_netdev_ops = {
+    .ndo_open        = labrador_eth_open
+    // .ndo_stop        = labrador_eth_close,
+    // .ndo_start_xmit        = labrador_eth_hard_start_xmit,
+    // .ndo_set_rx_mode    = labrador_eth_set_multicast_list,
+    // .ndo_do_ioctl        = labrador_eth_ioctl,
+    // .ndo_set_mac_address    = labrador_set_mac_address,
+    // .ndo_validate_addr    = eth_validate_addr,
+};
 
 static int labrador_eth_drv_probe(struct platform_device *pdev)
 {
@@ -196,10 +316,10 @@ static int labrador_eth_drv_probe(struct platform_device *pdev)
         goto err_out_iounmap;
     }
 
-    // /* Setup driver functions */
-    // ndev->netdev_ops = &labrador_netdev_ops;
-    // ndev->ethtool_ops = &labrador_eth_ethtool_ops;
-    // ndev->watchdog_timeo = msecs_to_jiffies(2500);
+    /* Setup driver functions */
+    ndev->netdev_ops = &labrador_netdev_ops;
+    ndev->ethtool_ops = &labrador_eth_ethtool_ops;
+    ndev->watchdog_timeo = msecs_to_jiffies(2500);
 
     /* Get size of DMA buffers/descriptors region */
     pldat->dma_buff_size = (TX_RING_SIZE + RX_RING_SIZE) * (ETH_PKG_MAX +
