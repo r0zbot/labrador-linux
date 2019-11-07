601699d7cd599a89477754506798d4cd04d75979
batmacumba <brunocarneirodacunha@usp.br>
2019-11-07 14:44:46 -0300

    tentativa gpio reset

--- linux/drivers/net/ethernet/caninos/labrador_eth.c
+++ linux/drivers/net/ethernet/caninos/labrador_eth.c
@@ -43,6 +43,10 @@
 #include <linux/of_net.h>
 #include <linux/types.h>
 
+#include <linux/gpio.h>
+#include <linux/of_platform.h>
+#include <linux/of_gpio.h>
+
 #include <mach/clkname.h>
 
 #define MODNAME "labrador-eth"
@@ -1050,6 +1054,88 @@ use_iram_for_net(struct device *dev)
     return false;
 }
 
+typedef struct{
+    int phy_reset_gpio;
+    int phy_power_gpio;
+}  phy_gpio;
+
+static int
+reset(struct platform_device *pdev)
+{
+    u32 cnt, phy_id;
+    INFO_MSG(" ethernet_phy_setup(void)");
+    static phy_gpio global_phy_gpio;
+    u16 reg_val;
+    int ret;
+
+    struct device * dev = &pdev->dev;
+
+    global_phy_gpio.phy_reset_gpio = 
+        of_get_named_gpio(dev->of_node, "phy-reset-gpio", 0);
+    
+    if (!gpio_is_valid(global_phy_gpio.phy_reset_gpio))
+    {
+        INFO_MSG("could not get reset gpio\n");
+        return -ENODEV;
+    }
+    global_phy_gpio.phy_power_gpio = 
+        of_get_named_gpio(dev->of_node, "phy-power-gpio", 0);
+    
+    if (!gpio_is_valid(global_phy_gpio.phy_power_gpio))
+    {
+        INFO_MSG("could not get power gpio\n");
+        return -ENODEV;
+    }
+    
+    ret = devm_gpio_request(dev, global_phy_gpio.phy_reset_gpio, "phy_reset");
+    
+    if (ret)
+    {
+        INFO_MSG("could not request reset gpio\n");
+        return ret;
+    }
+    ret = devm_gpio_request(dev, global_phy_gpio.phy_power_gpio, "phy_power");
+    
+    if (ret)
+    {
+        INFO_MSG("could not request power gpio\n");
+        return ret;
+    }
+    
+    
+    gpio_direction_output(global_phy_gpio.phy_reset_gpio, 0);
+    gpio_direction_output(global_phy_gpio.phy_power_gpio, 0);
+    gpio_set_value(global_phy_gpio.phy_power_gpio, 0);
+ 
+    gpio_set_value(global_phy_gpio.phy_reset_gpio, 0);
+    
+    if (gpio_is_valid(global_phy_gpio.phy_power_gpio))
+    { 
+        gpio_set_value(global_phy_gpio.phy_power_gpio, 1);  
+    }
+    else{
+        INFO_MSG("phy_power_gpio is no Valid");
+        return 1;
+    }
+    
+    if (gpio_is_valid(global_phy_gpio.phy_reset_gpio))
+    {       
+        gpio_set_value(global_phy_gpio.phy_reset_gpio, 1);
+        mdelay(150);//time for power up
+        gpio_set_value(global_phy_gpio.phy_reset_gpio, 0);
+        mdelay(12);//time for reset
+        gpio_set_value(global_phy_gpio.phy_reset_gpio, 1);
+
+        INFO_MSG("Reset_Done"); 
+    }else{
+        ERR_MSG("phy_reset_gpio is no Valid");
+        return -ENOMEM;
+    }
+    //time required to access registers
+    mdelay(150);
+    return 0;
+}
+
 static int 
 labrador_eth_drv_probe(struct platform_device *pdev)
 {
@@ -1061,6 +1147,8 @@ labrador_eth_drv_probe(struct platform_device *pdev)
     int irq, ret;
     u32 tmp;
     INFO_MSG("labrador_eth_drv_probe");
+
+    reset(pdev);
 
     /* Get platform resources */
     res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
